8c04041c2a82ff429b0b2cff59adedfd2b2e1f17
Georgi Kolev <georgipetrovkolev1998@gmail.com>
2024-06-24 11:24:32 +0300

    terraform state & simple network added

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "google" {
+  credentials = file("terraform-key.json")
+  project = var.project
+  region = var.region
+  zone = var.zone
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "terraformtest1-125453443"
+    prefix = "terraform1"
+    credentials = "terraform-key.json"
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
